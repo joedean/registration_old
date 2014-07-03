372cf0947163107dfd7596e47f41ca2a16368f51
Joe Dean <joe@on-site.com>
2014-07-02 19:33:46 -0700

    Fix broken tests caused by the user model and factory

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
 
   def role? ( role )
-    return !! self roles find_by_name ( role. to_s camelize )
+    return !! self.roles.find_by_name(role.to_s.camelize)
   end
 end
